06f69554862d0f7a455cf20bbc4ae4dfc9655c1d
Ian Jorgensen <jorgensen.ian@gmail.com>
2012-02-20 01:18:05 +0100

    second

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var server = require('router').create();
 var buffoon = require('buffoon');
 
 var readings = {};
+var content = {};
 var port = process.argv[2] || 9000;
 
 server.get('/', function(request, response) {
@@ -9,10 +10,18 @@ server.get('/', function(request, response) {
 	response.end(JSON.stringify(readings, null, '\t'));
 });
 
+server.get('/readings', function(request, response) {
+	response.writeHead(200, {'content-type':'application/json'});
+	response.end(JSON.stringify(content, null, '\t'));
+});
+
 server.post('/upload', function(request, response) {
 	buffoon.json(request, function(err, data) {
 		readings = data;
-		//console.log(new Buffer(, 'base64').toString('ascii'))
+		if (data.Attachments) {
+			content = new Buffer(data.Attachments.Content, 'base64').toString('ascii');
+		}
+		
 		response.writeHead(200);
 		response.end('ok');
 	});
